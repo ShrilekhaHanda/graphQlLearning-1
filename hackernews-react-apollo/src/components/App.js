7f5cb93f6b1c2a05f985d98c06d5b0c4ca0bab57
shrilekha8232 <shrilekha.handa@impactanalytis.co>
2019-12-03 12:54:03 +0530

    commit

---
+++ hackernews-react-apollo/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import '../styles/App.css';
+import LinkList from "./LinkList";
+
+
+class App extends React.Component {
+  render() {
+    return <LinkList />
+  }
+}
+
+export default App;
+
+
+// https://us1.prisma.sh/shreelihanda/grapgQlWork/graphQlWorkStage
